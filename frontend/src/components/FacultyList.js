a53a98533681948cf5429b67bb4ee1f25fe3c47b
Jon Blake <jon.kollari@mail.utoronto.ca>
2018-03-31 22:52:18 -0400

    budget functionality

--- frontend/src/components/FacultyList.js
+++ frontend/src/components/FacultyList.js
@@ -1,6 +1,7 @@
 // render columns based on props
 import React, { Component } from 'react';
 import { Row, Input, Button, Form, Table } from 'reactstrap';
+import $ from 'jquery'; 
 
 class FacultyList extends Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class FacultyList extends Component {
     render() {
         return (
             <div>
+                <h2> Grant Table</h2>
                 <Table>
                     <thead>
                         <tr key="facultyheader">
@@ -30,11 +32,28 @@ class FacultyList extends Component {
                     </thead>
                     <tbody>
                         {this.state.users.map(user =>
-                            <tr key={user.id}>
+                            <tr id="granter" key={user.id}>
                                 <td>{user.name}</td>
-                                <td><Input type="text"/></td>
-                                <td><Input type="text"/></td>
-                                <td><Button color="info">Grant</Button></td>
+                                <td id="r1"><Input type="number"/></td>
+                                <td id="r2"><Input type="number"/></td>
+                                <td id="r3"><Button color="info" onClick={() => runner(user.name)}>Grant</Button></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+                <h2> Edit Table</h2>
+                <Table>
+                    <thead>
+                        <tr key="facultyheader">
+                            <th>Full Name</th>
+                        </tr>
+                    </thead>
+                    <tbody id="editTable">
+
+                        {this.state.users.map(user =>
+                            <tr id="adder" key={user.id}>
+                            
+
                             </tr>
                         )}
                     </tbody>
@@ -42,6 +61,14 @@ class FacultyList extends Component {
             </div>
         );
     }
+}
+function runner (user) {
+    
+    var newtr1 = "<tr> <td>"+user+"</td>"
+    var newtr2 = "<td><Button color='info'>Edit</Button></td> </tr>"
+    $("#granter").remove();
+    $("#editTable").append(newtr1 + newtr2)
+
 }
 
 export default FacultyList;
